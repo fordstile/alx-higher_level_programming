7b6aa410dc8044323a3caf10c38d205ea2dda4b0
fordstile <masterland9772@gmail.com>
2024-01-08 09:06:35 -0800

    alx task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,24 +3,23 @@
 /**
  * reverse_listint - reverses a linked list
  * @head: pointer to the first node in the list
- *
  * Return: pointer to the first node in the new list
  */
 void reverse_listint(listint_t **head)
 {
-  listint_t *prev = NULL;
-  listint_t *current = *head;
-  listint_t *next = NULL;
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
 
-  while (current)
-    {
-      next = current->next;
-      current->next = prev;
-      prev = current;
-      current = next;
-    }
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
 
-  *head = prev;
+	*head = prev;
 }
 
 /**
@@ -31,69 +30,42 @@ void reverse_listint(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-  listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-  if (*head == NULL || (*head)->next == NULL)
-    return (1);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-  while (1)
-    {
-      fast = fast->next->next;
-      if (!fast)
+	while (1)
 	{
-	  dup = slow->next;
-	  break;
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-      if (!fast->next)
-	{
-	  dup = slow->next->next;
-	  break;
-	}
-      slow = slow->next;
-    }
 
-  reverse_listint(&dup);
+	reverse_listint(&dup);
 
-  while (dup && temp)
-    {
-      if (temp->n == dup->n)
+	while (dup && temp)
 	{
-	  dup = dup->next;
-	  temp = temp->next;
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
 	}
-      else
+
+	if (!dup)
+		return (1);
+
 	return (0);
-    }
-
-  if (!dup)
-    return (1);
-
-  return (0);
-}
-
-vi 100-print_python_list_info.c
-#include <Python.h>
-
-/**
- * print_python_list_info - Prints basic info about Python lists.
- * @p: A PyObject list.
- */
-void print_python_list_info(PyObject *p)
-{
-	int size, alloc, i;
-	PyObject *obj;
-
-	size = Py_SIZE(p);
-	alloc = ((PyListObject *)p)->allocated;
-
-	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", alloc);
-
-	for (i = 0; i < size; i++)
-	{
-		printf("Element %d: ", i);
-
-		obj = PyList_GetItem(p, i);
-		printf("%s\n", Py_TYPE(obj)->tp_name);
-	}
 }
