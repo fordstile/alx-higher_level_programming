63498fb83c71ab899767904ee2ff05dc15b6c02c
fordstile <masterland9772@gmail.com>
2024-01-23 10:57:30 -0800

    python classes

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,53 +1,82 @@
 #!/usr/bin/python3
-""" creates class Square """
+"""module for a singly linked list"""
 
 
-class Square:
-    """ Square class"""
-    def __init__(self, size=0):
-        if type(size) != int and type(size) != float:
-            raise TypeError("size must be a number")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+class Node:
+    """"defines a node"""
+
+    def __init__(self, data, next_node=None):
+        """initializes the node with instance variables"""
+
+        self.data = data
+        self.next_node = next_node
 
     @property
-    def size(self):
-        return self.__size
+    def data(self):
+        """gets data attribute"""
 
-    @size.setter
-    def size(self, value):
-        if type(value) != int and type(value) != float:
-            raise TypeError("size must be a number")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        return (self.__data)
 
-    def area(self):
-        return self.__size * self.__size
+    @data.setter
+    def data(self, value):
+        """sets data attribute"""
 
-    def __eq__(self, other):
-        """check if equal to another square"""
-        return(self.area() == other.area())
+        if not isinstance(value, int):
+            raise TypeError('data must be an integer')
+        self.__data = value
 
-    def __lt__(self, other):
-        """check if less than other square"""
-        return(self.area() < other.area())
+    @property
+    def next_node(self):
+        """get next_node attribute
+        Returns: next node
+        """
 
-    def __le__(self, other):
-        """check if less than or equal to other square"""
-        return(self.area() <= other.area())
+        return (self.__next_node)
 
-    def __ne__(self, other):
-        """check if not equal to another suqare"""
-        return(self.area() != other.area())
+    @next_node.setter
+    def next_node(self, value):
+        """set value of next node"""
 
-    def __gt__(self, other):
-        """check if greater than another square"""
-        return(self.area() > other.area())
+        if (value is not None and not isinstance(value, Node)):
+            raise TypeError('next_node must be a Node object')
 
-    def __ge__(self, other):
-        """check if greater than or equal to another square"""
-        return(self.area() >= other.area())
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """defines a singly linked list"""
+
+    def __init__(self):
+        """Initializes the singly linked list"""
+
+        self.head = None
+
+    def __str__(self):
+        """make list printable"""
+
+        printsll = ""
+        location = self.head
+        while location:
+            printsll += str(location.data) + "\n"
+            location = location.next_node
+        return printsll[:-1]
+
+    def sorted_insert(self, value):
+        """insert in a sorted fashion
+        Args:
+            value: what the value will be on the node
+        """
+        new = Node(value)
+        if not self.head:
+            self.head = new
+            return
+        if value < self.head.data:
+            new.next_node = self.head
+            self.head = new
+            return
+        location = self.head
+        while location.next_node and location.next_node.data < value:
+            location = location.next_node
+        if location.next_node:
+            new.next_node = location.next_node
+        location.next_node = new
