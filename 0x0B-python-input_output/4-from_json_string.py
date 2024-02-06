3c629c6dc0f0bf3a84ab7363a91fecee8ec42588
Fordstile <masterland9772@gmail.com>
2024-02-06 11:37:49 -0800

    updated

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
-"""class Student."""
+""" JSON-to-object function."""
+import json
 
 
-class Student:
-    """represents a student."""
-
-    def __init__(self, first_name, last_name, age):
-        """initializes a new Student.
-        Args:
-            first_name (str): the student first name.
-            last_name (str):the student last name.
-            age (int): The student age.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self):
-        """returns  dictionary representation of the Student class."""
-        return self.__dict__
+def from_json_string(my_str):
+    """function returns  Python object from a JSON string."""
+    return json.loads(my_str)
