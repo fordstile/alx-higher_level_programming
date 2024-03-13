572ccb9922bd08b2e73d109f3c7509a7f8c240b2
fordstile <masterland9772@gmail.com>
2024-03-13 15:02:44 -0700

    Providing Correct output

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- script that prints the full description of the table from the database in MYSQL server.
 SELECT *
-FROM information_schema.tables
+FROM information_schema.COLUMNS
 WHERE table_schema = 'hbtn_0c_0'
 AND table_name = 'first_table';
