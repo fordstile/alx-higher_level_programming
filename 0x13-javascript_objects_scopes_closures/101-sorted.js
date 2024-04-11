78f66d75b4a4c67315e92f60200c8a3206147855
fordstile <masterland9772@gmail.com>
2024-04-11 02:46:00 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const diction = require('./101-data').dict;
+const dict = require('./101-data.js').dict;
 const newDict = {};
-
-Object.keys(diction).map(function (key, index) {
-  if (newDict[diction[key]] === undefined) {
-    newDict[diction[key]] = [];
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
   }
-  newDict[diction[key]].push(key);
-});
-
+  newDict[dict[key]].push(key);
+}
 console.log(newDict);
