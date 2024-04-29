c054fb7509068beeab796c1b5fc2a025079ee08c
fordstile <masterland9772@gmail.com>
2024-04-29 04:50:44 -0700

    updated

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
+
 """
-This script defines a City class
-to work with MySQLAlchemy ORM.
+Defines a City model.
 """
 
-from relationship_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
-    """City class
-    Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The id of the class
-        name (str): The name of the class
-        state_id (int): The state the city belongs to
-    """
-    __tablename__ = 'cities'
+    """city for a MySQL database.
 
+    Attributes:
+        id : The city's id.
+        name : The city's name.
+        state_id: The city's state id.
+    """
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-This script defines a City class
-to work with MySQLAlchemy ORM.
+Defines State model.
 """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import Base, City
 
-from relationship_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey
 
+class State(Base):
+    """state for a MySQL database.
 
-class City(Base):
-    """City class
     Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The id of the class
-        name (str): The name of the class
-        state_id (int): The state the city belongs to
+        __tablename__ : The name of the MySQL table.
+        id : The state's id.
+        name : The state's name.
+        cities: The State-City relationship.
     """
-    __tablename__ = 'cities'
-
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
